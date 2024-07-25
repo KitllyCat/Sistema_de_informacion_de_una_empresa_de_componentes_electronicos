0926f0c83a07cc7968466bf19c710df7dca3c84f
lvelezm <lvelezm@unjbg.edu.pe>
2024-07-25 15:17:09 -0500

    Añade nueva función que muestra la lista de productos agregados, se creó la función verProductosAgregados(), la cual si es que no hay ningún producto agregado muestra un mensaje de que no hay ningún producto, sin embargo si es que si hay algunos pues por medio de un for con un i que inicia en cero y termina antes de la cantidad de productos muestra cada nombre del producto por medio del arreglo productos entrando en la cadena nombre del registro.

--- main.cpp
+++ main.cpp
@@ -33,6 +33,19 @@ void agregarProducto(){
     cout << "Producto agregado"<< endl;
 }
 
+void verProductosAgregados(){
+	if (cantidadProductos == 0){
+		cout << endl;
+		cout << "No hay productos agregados. "<< endl;
+		return;
+	}
+	for (int i = 0; i < cantidadProductos; i++){
+		cout << endl;
+		cout << "Producto N°" << i + 1 << ": " << productos[i].nombre << endl;
+		cout << "----------------------------" << endl;
+	}
+}
+
 
 void mostrarMenuBaseDeDatos(){
     do {
@@ -56,7 +69,7 @@ void mostrarMenuBaseDeDatos(){
 
         switch (opcionBaseDeDatos){
             case 'A':
-                //Añadir funcion para ver losproductos registrados 
+                verProductosAgregados(); //Añade funcion para ver los productos registrados 
                 break;
             case 'B':
                 //Añadir funcion para ver los productos ordenados alfabéticamente
