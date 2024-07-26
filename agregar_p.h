8403bad0023d9dc0bc9eeb73cd79c98e22290361
lvelezm <lvelezm@unjbg.edu.pe>
2024-07-26 00:39:28 -0500

    Añade el archivo de encabezado agregar_p.h para la funcionalidad de agregar productos. Este archivo declara la función agregarProducto y utiliza mostrar_p.h para incluir las definiciones compartidas de Producto, productos[] y cantidadProductos.

---
+++ agregar_p.h
@@ -0,0 +1,12 @@
+#ifndef AGREGAR_P
+#define AGREGAR_P
+#include <iostream>
+#include <string>
+#include "mostrar_p.h"
+using namespace std;
+extern Producto productos[];
+extern int cantidadProductos;
+
+void agregarProducto();
+
+#endif
