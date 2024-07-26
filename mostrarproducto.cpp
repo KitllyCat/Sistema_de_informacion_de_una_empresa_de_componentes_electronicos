efc33ede322f19d99a989ddf0c25a454eb98d555
lvelezm <lvelezm@unjbg.edu.pe>
2024-07-26 18:57:33 -0500

    Se implementó la función verProductosAgregadosPrecio() en mostrarproducto.cpp. Esta función ordena los productos por su precio de menor a mayor utilizando el algoritmo de burbuja y luego los muestra en ese orden.

--- mostrarproducto.cpp
+++ mostrarproducto.cpp
@@ -39,3 +39,33 @@ void verProductosAgregadosAlf() {
         cout << "----------------------------" << endl;
     }
 }
+
+void verProductosAgregadosPrecio() {
+    if (cantidadProductos == 0) {
+        cout << endl;
+        cout << "No hay productos agregados." << endl;
+        return;
+    }
+    
+    Producto productosOrdenados[100];
+    for (int i = 0; i < cantidadProductos; i++) {
+        productosOrdenados[i] = productos[i];
+    }
+
+    for (int i = 0; i < cantidadProductos - 1; i++) {
+        for (int j = 0; j < cantidadProductos - i - 1; j++) {
+            if (productosOrdenados[j].precioPromedio > productosOrdenados[j + 1].precioPromedio) {
+                Producto temp = productosOrdenados[j];
+                productosOrdenados[j] = productosOrdenados[j + 1];
+                productosOrdenados[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < cantidadProductos; i++) {
+        cout << endl;
+        cout << "Producto N°" << i + 1 << ": " << productosOrdenados[i].nombre << endl;
+        cout << "Precio Promedio: " << productosOrdenados[i].precioPromedio << endl;
+        cout << "----------------------------" << endl;
+    }
+}
