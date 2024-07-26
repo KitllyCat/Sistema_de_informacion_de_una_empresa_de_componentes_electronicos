37f367763daf32213c8e3fd7b324a49796b901de
lvelezm <lvelezm@unjbg.edu.pe>
2024-07-25 23:20:11 -0500

    Se elimino la función mostrar productos para ser reemplazada por una libreria o archivo de encabezado que hace lo mismo

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 #include <windows.h> //libreria para las tildes
+#include "mostrar_p.h"
 using namespace std;
 char opcionPrincipal, opcionBaseDeDatos, opcionUsuario;
-
-struct Producto {
-	string nombre;
-	float precioPromedio;
-};
-
-Producto productos[100];
-int cantidadProductos = 0; 	
 
 void mostrarMenuPrincipal(){
     cout << "----- \"N-Tek\" -----" << endl;
@@ -32,20 +25,6 @@ void agregarProducto(){
     cout << endl;
     cout << "Producto agregado"<< endl;
 }
-
-void verProductosAgregados(){
-	if (cantidadProductos == 0){
-		cout << endl;
-		cout << "No hay productos agregados. "<< endl;
-		return;
-	}
-	for (int i = 0; i < cantidadProductos; i++){
-		cout << endl;
-		cout << "Producto N°" << i + 1 << ": " << productos[i].nombre << endl;
-		cout << "----------------------------" << endl;
-	}
-}
-
 
 void mostrarMenuBaseDeDatos(){
     do {
