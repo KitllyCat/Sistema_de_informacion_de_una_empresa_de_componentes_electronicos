975f296da0a9bb456866e441e152b73e04054b47
lvelezm <lvelezm@unjbg.edu.pe>
2024-07-26 18:25:03 -0500

    Se implementó la función verProductosAgregadosAlf() en mostrarproducto.cpp. Esta función ordena los productos por su nombre utilizando el algoritmo de burbuja y luego los muestra en orden alfabético.

--- mostrarproducto.cpp
+++ mostrarproducto.cpp
@@ -13,3 +13,29 @@ void verProductosAgregados() {
         cout << "----------------------------" << endl;
     }
 }
+
+void verProductosAgregadosAlf() {
+    if (cantidadProductos == 0) {
+        cout << endl;
+        cout << "No hay productos agregados." << endl;
+        return;
+    }
+    Producto productosOrdenados[100];
+    for (int i = 0; i < cantidadProductos; i++) {
+        productosOrdenados[i] = productos[i];
+    }
+    for (int i = 0; i < cantidadProductos - 1; i++) {
+        for (int j = 0; j < cantidadProductos - i - 1; j++) {
+            if (productosOrdenados[j].nombre > productosOrdenados[j + 1].nombre) {
+                Producto temp = productosOrdenados[j];
+                productosOrdenados[j] = productosOrdenados[j + 1];
+                productosOrdenados[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < cantidadProductos; i++) {
+        cout << endl;
+        cout << "Producto N°" << i + 1 << ": " << productosOrdenados[i].nombre << endl;
+        cout << "----------------------------" << endl;
+    }
+}
