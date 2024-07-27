67e1e277b4d0c27ec556da37961fb21ccae5d554
KitllyCat <cjchambem@unjbg.edu.pe>
2024-07-26 22:04:33 -0500

    Se añadio la cabecera para las librerias que contendran las funciones para el main

---
+++ Menusuario.h
@@ -0,0 +1,10 @@
+#ifndef MENUSUARIO_H
+#define MENUSUARIO_H
+
+void mostrarProductos();
+void mostrarProductosPorMarca();
+void mostrarProductosPorPrecio();
+void adquirirNuevoProducto();
+void dejarRecomendacion();
+
+#endif
