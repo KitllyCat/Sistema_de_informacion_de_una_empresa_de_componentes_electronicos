70aa93ca517d120224ebc84022483df675975687
KitllyCat <cjchambem@unjbg.edu.pe>
2024-07-28 21:03:31 -0500

    Se añadio colores a la funcion

--- agregarproducto.cpp
+++ agregarproducto.cpp
@@ -1,18 +1,19 @@
 #include "agregar_p.h"
+#include "colors.h"
 void agregarProducto(){
-    cout << "Ingrese el nombre del producto: ";
+    cout << LGREEN<<"Ingrese el nombre del producto: ";
     cin.ignore();
     getline(cin, productos[cantidadProductos].nombre);
-    cout << "Ingrese el precio en promedio del producto: ";
+    cout << LGREEN<<"Ingrese el precio en promedio del producto: ";
     cin >> productos[cantidadProductos].precioPromedio;
     cantidadProductos++;
     cout << endl;
-    cout << "Producto agregado"<< endl;
+    cout << GREEN<<"Producto agregado"<< endl;
 }
 
 void eliminarProducto() {
     string nombre;
-    cout << "Ingrese el nombre del producto a eliminar: ";
+    cout <<LGREEN<< "Ingrese el nombre del producto a eliminar: ";
     cin >> nombre;
 
     for (int i = 0; i < cantidadProductos; ++i) {
@@ -21,31 +22,31 @@ void eliminarProducto() {
                 productos[j] = productos[j + 1];
             }
             cantidadProductos--;
-            cout << "Producto eliminado con éxito." << endl;
+            cout << GREEN<<"Producto eliminado con éxito." << endl;
             return;
         }
     }
 
-    cout << "Producto no encontrado." << endl;
+    cout <<RED<<"Producto no encontrado." << endl;
 }
 
 void actualizarProducto() {
 	string nombreActualizar;
-    cout << "Ingrese el nombre del producto a actualizar: ";
+    cout <<LGREEN<<"Ingrese el nombre del producto a actualizar: ";
     cin.ignore();
     getline(cin, nombreActualizar);
     bool encontrado = false;
     for (int i = 0; i < cantidadProductos; i++) {
         if (productos[i].nombre == nombreActualizar) {
-            cout << "Ingrese el nuevo precio del producto: ";
+            cout << LGREEN<<"Ingrese el nuevo precio del producto: ";
             cin >> productos[i].precioPromedio;
             cout<<endl;
-            cout << "Producto actualizado con éxito!" << endl;
+            cout <<GREEN<<"Producto actualizado con éxito!" << endl;
             encontrado = true;
             break;
         }
     }
     if (!encontrado) {
-        cout << "Producto no encontrado." << endl;
+        cout <<RED<< "Producto no encontrado." << endl;
     }
 }
