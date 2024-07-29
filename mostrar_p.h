cdfe4b2fa7572a30cd07db2fda42edf0a791bc11
KitllyCat <cjchambem@unjbg.edu.pe>
2024-07-28 19:40:21 -0500

    Se añadio el encabezado de mostrarProductos

---
+++ mostrar_p.h
@@ -0,0 +1,18 @@
+#ifndef MOSTRAR_P
+#define MOSTRAR_P
+#include <iostream>
+#include <string>
+using namespace std;
+struct Producto{
+    string nombre;
+    float precioPromedio;
+};
+
+extern Producto productos[];
+extern int cantidadProductos;
+
+void verProductosAgregados();
+void verProductosAgregadosAlf();
+void verProductosAgregadosPrecio();
+
+#endif 
